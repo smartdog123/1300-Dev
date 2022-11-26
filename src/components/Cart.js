785cadb84e31ea0308411da04d612dcc47e885c3
smartdog123 <115118215+smartdog123@users.noreply.github.com>
2022-11-25 23:11:31 -0500

    Add files via upload

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,19 +1,19 @@
 import React from "react";
-import CartItem from './CartItem';
+import CartItem from "./CartItem";
+import "./Cart.css";
 
 class Cart extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Cart</h1>
-                {Object.keys(this.props.state.items).map((key) => (
-                    <CartItem count={this.props.state.items[key]} name={key}></CartItem>
-                ))}
-                <h3>Total: ${this.props.state.price.toFixed(2)}</h3>
-            </div>
-        );
-    }
-
+  render() {
+    return (
+      <div className="cart">
+        <h1>Cart</h1>
+        {Object.keys(this.props.state.items).map((key) => (
+          <CartItem count={this.props.state.items[key]} name={key}></CartItem>
+        ))}
+        <h3>Total: ${this.props.state.price.toFixed(2)}</h3>
+      </div>
+    );
+  }
 }
 
 export default Cart;
